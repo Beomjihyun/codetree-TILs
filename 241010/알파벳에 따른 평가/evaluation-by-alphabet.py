bcab6e9b7baaa714e476aa8706c54a107f74777e
Beomjihyun <92281591+Beomjihyun@users.noreply.github.com>
2024-10-10 14:53:38 +0900

    Title: 알파벳에 따른 평가, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241010/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+m=input()
+
+if m == 'S':
+    print("Superior")
+elif m == 'A':
+    print("Excellent")
+elif m == 'B':
+    print("Good")
+elif m == 'C':
+    print("Usually")
+elif m == 'D':
+    print("Effort")
+else:
+    print("Failure")
