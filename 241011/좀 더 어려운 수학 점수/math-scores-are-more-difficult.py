f6ac63694ab659e8d5e2e233692c049599abc212
Beomjihyun <92281591+Beomjihyun@users.noreply.github.com>
2024-10-11 15:19:45 +0900

    Title: 좀 더 어려운 수학 점수, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 241011/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 241011/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -1,15 +1,24 @@
 arr_a = input().split()
 arr_b = input().split()
 
-a_m = int(arr_a[0])
-a_e = int(arr_a[1])
+# a_m = int(arr_a[0])
+# a_e = int(arr_a[1])
 
-b_m = int(arr_b[0])
-b_e = int(arr_b[1])
+# b_m = int(arr_b[0])
+# b_e = int(arr_b[1])
 
-if a_m > b_m:
-    print("A")
-elif a_m == b_m and a_e > b_e:
+a_m, a_e = int(arr_a[0]), int(arr_a[1])
+
+b_m, b_e = int(arr_b[0]),int(arr_b[1])
+
+# if a_m > b_m:
+#     print("A")
+# elif a_m == b_m and a_e > b_e:
+#     print("A")
+# else:
+#     print("B")
+
+if a_m > b_m or (a_m == b_m and a_e > b_e):
     print("A")
 else:
     print("B")
